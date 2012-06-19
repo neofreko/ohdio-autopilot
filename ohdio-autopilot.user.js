4428729d1863703011db781fc50c075927e7ba35
Akhmad Fathonih <akhmadf@gmail.com>
2012-06-20 02:30:07 +0900

    Now, we can scrobble too. yay!

--- ohdio-autopilot.user.js
+++ ohdio-autopilot.user.js
@@ -7,19 +7,40 @@
 //@copyright  2012+, Akhmad Fathonih
 //@require https://raw.github.com/fxb/javascript-last.fm-api/master/lastfm.api.md5.js
 //@require https://raw.github.com/fxb/javascript-last.fm-api/master/lastfm.api.cache.js
-//@require https://github.com/neofreko/javascript-last.fm-api/raw/master/lastfm.api.js
+//-require http://localhost/lstfm.js
+//@require https://github.com/neofreko/javascript-last.fm-api/raw/master/lastfm.api.js?nocachepuhlease
 //@require http://code.jquery.com/jquery-1.7.2.min.js
 //==/UserScript==
 
 /* Create a cache object */
 var cache = new LastFMCache();
+apiKey = '814a8780d36924ce2941b3d453211dcf';
+apiSecret = '51ba653062842d2a6eceae165af61bf9';
+global_username = prompt('For scroblling function, Ohd.io autopilot needs your last.fm username');
+global_password = prompt('For scroblling function, Ohd.io autopilot needs your last.fm password');;
+global_session = null;
+global_added = 0;
+global_manual_play = 0;
 
 /* Create a LastFM object */
 var lastfm = new LastFM({
-    apiKey    : '814a8780d36924ce2941b3d453211dcf',
-    apiSecret : '51ba653062842d2a6eceae165af61bf9',
+    apiKey    : apiKey,
+    apiSecret : apiSecret,
     //cache     : cache; // causing troubles when lastfm func called within onPlaylistItem. undefined method: getObject
 });
+
+/*token = /token=(.*?)(&.*)?$/.exec(location.search);
+console.log('haz token', token)
+    alert('stop');
+if (token) {
+    // 
+    lastfm.auth.getSession({token: token[1]}, function(data) {
+        console.log('session?',data);
+    })
+} else
+    // init web auth
+    GM_openInTab('http://www.last.fm/api/auth/?api_key=' + apiKey);
+*/
 
 function isjwplayer() { return typeof unsafeWindow.jwplayer == 'function';}
 function jwplayer() {return unsafeWindow.jwplayer();}
@@ -33,10 +54,6 @@ $(function () {
         }
     }, 1000);
 });
-
-//window.setTimeout(initOhdioAddon, 5000);
-global_added = 0;
-global_manual_play = 0;
 
 function searchAndAdd(artist) {
     $.ajax({
@@ -95,17 +112,37 @@ unsafeWindow.handlePlayItem = function() {
         track = jwplayer().getPlaylistItem();
         console.log("will grep similar track or artist from last.fm. artist: ", track.song.artist, ' song: ', track.song.song);
         //console.log(track);
-        lastfm.artist.getSimilar({artist: track.song.artist, limit: 20}, {success: unsafeWindow.handleSimilarArtist, error: function (code, message) {console.log('error', message )}})
+        lastfm.artist.getSimilar({artist: track.song.artist, limit: 20}, 
+                                 {success: unsafeWindow.handleSimilarArtist, error: function (code, message) {console.log('error', message )}})
         global_manual_play = 0; // reset so onPlay can respond to track changes
 }
 
 function initOhdioAddon() {
     console.log('Yay, Ohdio!');
     unsafeWindow.jwplayer().onPlaylistItem(function(){global_manual_play = 1; unsafeWindow.handlePlayItem()})
-        //.onPlay(function() {if (!global_manual_play) unsafeWindow.handlePlayItem()});
+        .onPlay(function() {
+            track = jwplayer().getPlaylistItem();
+            console.log('Update now playing on last.fm:', track)
+                lastfm.track.updateNowPlaying({artist: track.song.artist, track: track.song.song, album: track.song.album}, global_session, 
+                                              {success: unsafeWindow.lastfmFoo, error: unsafeWindow.lastfmError})
+            console.log('Scrobbling:', track)
+                lastfm.track.scrobble([{'artist': track.song.artist, 'track': track.song.song, 'timestamp': Math.round((new Date()).getTime() / 1000)}], global_session, 
+                                              {success: unsafeWindow.lastfmFoo, error: unsafeWindow.lastfmError})
+        });
 }
 
 //console.log('unsafeWindow', typeof unsafeWindow);
 unsafeWindow.lastfmFoo = function(data){
         console.log('debug', data);
     }
+    
+unsafeWindow.lastfmError = function(code, message){
+        console.log('error', code, message);
+    }
+//GM_log(lastfm.auth.getMobileSession)
+    lastfm.auth.getMobileSession({username: global_username, password: global_password}, {
+        success: function (data) {
+            global_session = data.session;
+            console.log('got session:', global_session)
+    }, 
+        error: unsafeWindow.lastfmError})
